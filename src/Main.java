ad1952947375bae701b4dbe48f92c8840e0166e9
V Manin <victormanin151@gmail.com>
2025-07-06 13:46:38 +0300

    fixed a bit the start menu

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         boolean isRunning = true;
-        int choice;
 
 
         while(isRunning){
@@ -18,30 +17,50 @@ public class Main {
             System.out.println("3 - Edit a car.");
             System.out.println("4 - List all cars.");
             System.out.println("5 - Search for a specific car.");
-            System.out.println("6 - Remove a specific car.");
-            System.out.println("0 - Exit the program!");
-            System.out.println("*********************");
-            System.out.print("Enter you choice (0-6): ");
+            System.out.println("6 - Return a car.");
+            System.out.println("7 - Remove a specific car.");
+            System.out.println("0 - Save & Exit the program!");
 
-            choice = Integer.parseInt(s.nextLine());
+            int choice = -1;
+            boolean validInput = false;
 
+            while (!validInput) {
+                System.out.println("**************************");
+                System.out.print("Select from the menu (0–7): ");
+                String input = s.nextLine();
+                try {
+                    choice = Integer.parseInt(input);
+                    if(choice >= 0 && choice < 8){
+                        validInput = true;
+                    }
+                } catch (NumberFormatException ignored) {
+                    }
 
-            switch (choice){
+                    if (!validInput) {
+                        System.out.println("**************************");
+                        System.out.println("Invalid input — please enter a number between 0 and 7.");
+                    }
+            }
+
+            switch (choice) {
 //                case 1 ->
 //                case 2 ->
 //                case 3 ->
 //                case 4 ->
 //                case 5 ->
 //                case 6 ->
-                case 0 -> isRunning = false;
-                default -> System.out.println("Invalid choice");
+                case 7 -> System.out.println("Remove a specific car (coming soon).");
+                case 0 -> {
+                    isRunning = false;
+                    System.out.println("Saving and exiting the program...");
+                }
             }
 
             System.out.println("**************************");
-            System.out.println("Thank you! Have a nice day!");
+            System.out.println("Thank you for using Car Rental System!");
+            System.out.println("Have a nice day!");
             System.out.println("**************************");
-
-            s.close();
         }
+        s.close();
     }
 }
