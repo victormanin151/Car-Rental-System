e7c06ec7b92d3972387a947ce623c1924f20fcc4
V Manin <victormanin151@gmail.com>
2025-07-06 19:41:44 +0300

    minor fixes in customer

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -6,7 +6,6 @@ import static services.CarService.getCarByID;
 public class Customer {
     private String firstName;
     private String lastName;
-    private String userName;
     private String startDate;
     private String returnDate;
     private Cars chosenCar;
@@ -20,47 +19,47 @@ public class Customer {
         this.chosenCar = getCarByID(carID);
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
 
     public String getFullName() {
         return firstName + " " + lastName;
     }
 
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getReturnDate() {
-        return returnDate;
-    }
-
-    public void setReturnDate(String returnDate) {
-        this.returnDate = returnDate;
-    }
-
-    public Cars getChosenCar() {
-        return chosenCar;
-    }
-
-    public void setChosenCar(Cars chosenCar) {
-        this.chosenCar = chosenCar;
-    }
+//    public String getStartDate() {
+//        return startDate;
+//    }
+//
+//    public void setStartDate(String startDate) {
+//        this.startDate = startDate;
+//    }
+//
+//    public String getReturnDate() {
+//        return returnDate;
+//    }
+//
+//    public void setReturnDate(String returnDate) {
+//        this.returnDate = returnDate;
+//    }
+//
+//    public Cars getChosenCar() {
+//        return chosenCar;
+//    }
+//
+//    public void setChosenCar(Cars chosenCar) {
+//        this.chosenCar = chosenCar;
+//    }
 }
